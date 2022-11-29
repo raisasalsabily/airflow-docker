44b03d1b9e934a2b1f2b77cec6b774c4d75a4dd2
raisasalsabily <raisa.salsabil@mail.ugm.ac.id>
2022-11-29 23:00:18 +0700

    fix: update twitter_etl.py

--- etl/twitter_etl.py
+++ etl/twitter_etl.py
@@ -6,6 +6,7 @@ import s3fs
 
 def run_twitter_etl():
 
+    # access key dari twitter
     a_key = 'cyXcjxUc1ZlLD2v1DjaJrLiVI' 
     a_secret = 'pD3mk041ROBiSSwt2C3kytR9eF1zYrX1vBD8whpDln5Zq4y1jX'
     c_key = '1279253779540410373-8ODCqefNcIvSFXSrwKH7DPhtQTda3i' 
@@ -17,24 +18,23 @@ def run_twitter_etl():
 
     # Buat objek API 
     api = tweepy.API(auth)
-    tweets = api.user_timeline(screen_name='@ugm_fess', 
-                            count=15, # max=200
-                            include_rts = False,
-                            # penting untuk keep full_text, jika tidak maka hanya 140 kata pertama yang diekstrak 
-                            tweet_mode = 'extended'
-                            )
+    tweets = api.search_tweets(
+                            q="ganjar",             # keyword yang dicari
+                            lang="id",              # bahasa yang dicari (Indonesia)
+                            result_type="recent",   # tweet yang diambil yang terkini
+                            count=100               # max = 100 baris tweets
+                        )
 
     list = []
     for tweet in tweets:
-        text = tweet._json["full_text"]
+        search_data = {
+                    'text': tweet.text, # cukup scrape text dari tweet agar tidak perlu cleaning berlebihan
+                    }
+        list.append(search_data)
 
-        refined_tweet = {"user": tweet.user.screen_name,
-                        'text' : text,
-                        'favorite_count' : tweet.favorite_count,
-                        'retweet_count' : tweet.retweet_count,
-                        'created_at' : tweet.created_at}
-            
-        list.append(ugm_fess_data)
+    new_tweets_df = pd.DataFrame(list) # masukkan ke dataframe
 
-    df = pd.DataFrame(list)
-    df.to_csv('ugm_fess_data.csv')
+    ## untuk load pertama
+    # new_tweets_df.to_csv('tweets.csv', index=False)
+    ## untuk append data frame ke CSV file yang sudah ada
+    new_tweets_df.to_csv('tweets.csv', mode='a', index=False, header=False)
